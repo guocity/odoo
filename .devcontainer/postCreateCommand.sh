88468f134a3d2443f866c8e8cade19d445846e97
L <guocity@users.noreply.github.com>
2025-05-07 14:44:20 -0400

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y libldap2-dev libsasl2-dev postgresql postgresql-client libpq-dev
-sudo service postgresql start
+set -e # exit immediately if a command exits with a non-zero status.
+set -x # enables debug mode, which prints each command to standard error before executing it.
+
+# Retry APT install up to 2 times
+for i in 1 2; do
+    # Clear APT cache and update
+    sudo apt-get clean
+    sudo rm -rf /var/lib/apt/lists/*
+    sudo apt-get update --fix-missing
+
+    # Fix broken dependencies
+    sudo apt --fix-broken install -y
+    sudo apt-get install -y --fix-missing \
+        libldap2-dev libsasl2-dev postgresql postgresql-client libpq-dev && break
+    echo "APT install failed, retrying ($i/2)..."
+    sleep 1
+done
+
 pip install uv
 sudo uv pip install --user -r requirements.txt || echo 'No requirements.txt found, skipping dependency installation'
